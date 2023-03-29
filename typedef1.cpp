5070d99081c045273dbdd06cea8326b5b4ba6cc1
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-03-29 00:28:38 -0600

    Create typedef1.cpp

---
+++ typedef1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using std::cin;     using std::sort;
+using std::cout;    using std::streamsize;
+using std::endl;    using std::string;
+using std::setprecision;    using std::vector;
+
+int main()
+{
+    typedef int Age;
+
+    //make more ints using Age as the reference
+    Age adult;
+    Age teen;
+    Age child;
+
+    cout<<"Enter the age for an adult: ";
+    cin>>adult;
+
+
+
+    return 0;
+}

9e33ace3e16f1485e825a55946ea5500a544c7e9
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-04-01 19:40:03 -0600

    constructors with parameters
    usage and initialization

---
+++ construcP.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <array>
+#include <cassert>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::array;
+
+//Constructors with parameters
+
+class Fraction
+{
+private:
+    int pnumerator{};
+    int pdenominator{};
+
+public:
+    Fraction() //default constructor
+    {
+       pnumerator = 0;
+       pdenominator = 1;
+    }
+
+    Fraction(int num, int den) // constructor with two parameters, one having a default value
+    {
+        assert(denominator != 0); //exits of condition is false
+        pnumerator = num;
+        pdenominator = den;
+    }
+    int getNumerator() { return pnumerator; }
+    int getDenominator() { return pdenominator; }
+    double getValue() { return static_cast<double>(pnumerator) / pdenominator; }
+
+};
+
+//by having two constructors a default c. will be called in the default case
+//while a second constructor takes two parameters
+
+int main()
+{
+    //using a constructor with parameters
+    Fraction fiveThirds{5,3}; //list initialization
+    Fraction threeQuarters(3,4); //direct init.
+    
+    Fraction six{6}; //still calls Fraction(int, int) constructor, 
+    //second parameter uses default value of 1
+
+
+    return 0;
+
+}
+
+
